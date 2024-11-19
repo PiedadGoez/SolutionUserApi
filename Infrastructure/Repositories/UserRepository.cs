d3ed94d6d6861195d280370376db8fad81e23bb5
PiedadGoez <piedadgoezarismendy@gmail.com>
2024-11-19 14:43:06 -0500

    agrego excepciones

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -19,8 +19,17 @@ namespace Infrastructure.Repositories
 
         public async Task AddAsync(User user, CancellationToken cancellationToken)
         {
-            await _dbContext.Users.AddAsync(user, cancellationToken);
-            await _dbContext.SaveChangesAsync(cancellationToken);
+            try
+            {
+                await _dbContext.Users.AddAsync(user, cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+
+                throw new ApplicationException("error en metodo AddAsync", ex);
+            }
+            
         }
 
         public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
@@ -37,51 +46,75 @@ namespace Infrastructure.Repositories
 
         public async Task<Pagination<User>> GetByFilterAsync(string? firstName, string? lastName, int page, int pageSize, CancellationToken cancellationToken)
         {
-            IQueryable<User> query = _dbContext.Users;
+            try
+            {
+                IQueryable<User> query = _dbContext.Users;
 
-            if (!string.IsNullOrEmpty(firstName))
-                query = query.Where(u => u.FirstName.Contains(firstName, StringComparison.OrdinalIgnoreCase));
+                if (!string.IsNullOrEmpty(firstName))
+                    query = query.Where(u => u.FirstName.ToUpper().Contains(firstName.ToUpper()));
 
-            if (!string.IsNullOrEmpty(lastName))
-                query = query.Where(u => u.LastName.Contains(lastName, StringComparison.OrdinalIgnoreCase));
+                if (!string.IsNullOrEmpty(lastName))
+                    query = query.Where(u => u.LastName.ToUpper().Contains(lastName.ToUpper()));
 
-            var totalRecords = await query.CountAsync(cancellationToken);
-            var items = await query
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .ToListAsync(cancellationToken);
+                var totalRecords = await query.CountAsync(cancellationToken);
+                var items = await query
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken);
 
-            return new Pagination<User>(items, totalRecords, page, pageSize);
+                return new Pagination<User>(items, totalRecords, page, pageSize);
+            }
+            catch (Exception ex)
+            {
+
+                throw new ApplicationException("error en GetByFilterAsync", ex);
+            }
+           
         }
 
         public async Task<bool> UpdateAsync(User user, CancellationToken cancellationToken)
         {
-
-            var User = await _dbContext.Users.Where(x => x.Id == user.Id).FirstOrDefaultAsync();
-
-            if (user == null)
+            try
             {
-                return false;
-            }
+                var User = await _dbContext.Users.Where(x => x.Id == user.Id).FirstOrDefaultAsync();
 
-            _dbContext.Users.Update(user);
-            await _dbContext.SaveChangesAsync(cancellationToken);
-            return true;
+                if (user == null)
+                {
+                    return false;
+                }
+
+                _dbContext.Users.Update(user);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("error en UpdateAsync", ex);
+            }
+           
         }
 
         public async Task<bool> DeleteUserAsync(Guid userId, CancellationToken cancellationToken)
         {
-            var user = await _dbContext.Users.FindAsync(new object[] { userId }, cancellationToken);
+            try
+            {
+                var user = await _dbContext.Users.FindAsync(new object[] { userId }, cancellationToken);
 
-            if (user == null)
-            {               
-                return false;
+                if (user == null)
+                {
+                    return false;
+                }
+
+                _dbContext.Users.Remove(user);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+                return true;
             }
-
-            _dbContext.Users.Remove(user);
-            await _dbContext.SaveChangesAsync(cancellationToken); 
-
-            return true; 
+            catch (Exception ex)
+            {
+                throw new ApplicationException("error en DeleteUserAsync", ex);
+            }
+           
         }
     }
 }
